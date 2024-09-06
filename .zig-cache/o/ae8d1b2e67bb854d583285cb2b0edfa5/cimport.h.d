2f9a5f3b2bcc3e18ae3a3a5fb9d53b26b7503db1
valbi <valbian@gmail.com>
2024-09-06 16:06:34 +0800

    revert set_zero() which hurts performance

---
+++ .zig-cache/o/ae8d1b2e67bb854d583285cb2b0edfa5/cimport.h.d
@@ -0,0 +1,20 @@
+cimport.o: \
+  D:/Code/Zig/brainfuck-interpreter/.zig-cache/o/ae8d1b2e67bb854d583285cb2b0edfa5/cimport.h \
+  "C:/Program Files/Zig/zigup/zig/0.13.0-dev.351+64ef45eb0/files/lib/libc/include/any-windows-any/conio.h" \
+  "C:/Program Files/Zig/zigup/zig/0.13.0-dev.351+64ef45eb0/files/lib/libc/include/any-windows-any/crtdefs.h" \
+  "C:/Program Files/Zig/zigup/zig/0.13.0-dev.351+64ef45eb0/files/lib/libc/include/any-windows-any/corecrt.h" \
+  "C:/Program Files/Zig/zigup/zig/0.13.0-dev.351+64ef45eb0/files/lib/libc/include/any-windows-any/_mingw.h" \
+  "C:/Program Files/Zig/zigup/zig/0.13.0-dev.351+64ef45eb0/files/lib/libc/include/any-windows-any/_mingw_mac.h" \
+  "C:/Program Files/Zig/zigup/zig/0.13.0-dev.351+64ef45eb0/files/lib/libc/include/any-windows-any/_mingw_secapi.h" \
+  "C:/Program Files/Zig/zigup/zig/0.13.0-dev.351+64ef45eb0/files/lib/include/vadefs.h" \
+  "C:/Program Files/Zig/zigup/zig/0.13.0-dev.351+64ef45eb0/files/lib/libc/include/any-windows-any/vadefs.h" \
+  "C:/Program Files/Zig/zigup/zig/0.13.0-dev.351+64ef45eb0/files/lib/libc/include/any-windows-any/sdks/_mingw_ddk.h" \
+  "C:/Program Files/Zig/zigup/zig/0.13.0-dev.351+64ef45eb0/files/lib/libc/include/any-windows-any/corecrt_stdio_config.h" \
+  "C:/Program Files/Zig/zigup/zig/0.13.0-dev.351+64ef45eb0/files/lib/include/stddef.h" \
+  "C:/Program Files/Zig/zigup/zig/0.13.0-dev.351+64ef45eb0/files/lib/include/__stddef_ptrdiff_t.h" \
+  "C:/Program Files/Zig/zigup/zig/0.13.0-dev.351+64ef45eb0/files/lib/include/__stddef_size_t.h" \
+  "C:/Program Files/Zig/zigup/zig/0.13.0-dev.351+64ef45eb0/files/lib/include/__stddef_wchar_t.h" \
+  "C:/Program Files/Zig/zigup/zig/0.13.0-dev.351+64ef45eb0/files/lib/include/__stddef_null.h" \
+  "C:/Program Files/Zig/zigup/zig/0.13.0-dev.351+64ef45eb0/files/lib/include/__stddef_max_align_t.h" \
+  "C:/Program Files/Zig/zigup/zig/0.13.0-dev.351+64ef45eb0/files/lib/include/__stddef_offsetof.h" \
+  "C:/Program Files/Zig/zigup/zig/0.13.0-dev.351+64ef45eb0/files/lib/libc/include/any-windows-any/sec_api/conio_s.h"
